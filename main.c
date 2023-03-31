5cec68b2bb453e913ca40676dc7bf8d4b5e446e2
madsvnielsen <mads@hvn.dk>
2023-03-31 02:37:05 +0200

    - More changes - Comments

--- main.c
+++ main.c
@@ -2,9 +2,12 @@
 #include <stdlib.h>
 #include <stdbool.h>
 #include <string.h>
+#include <unistd.h>
 
+//enum suitType with the different suits
 enum suitType{hearts, diamonds, spades, clubs};
 
+//struct nodes holds cardValue, if its visible, the suit, a pointer to the next card in the pile
 struct node {
     int cardValue;
     bool visible;
@@ -12,11 +15,13 @@ struct node {
     struct node *next;
 };
 
+//struct linkedList (holds a pointer to the head of a node(card)
 struct linkedList{
     struct node *head;
 };
 
 
+//Prints list (for debugging)
 void printLinkedList(struct linkedList list){
     struct node *el = list.head;
     char suitNames[4][10] = {"hearts","diamonds","spades","clubs"};
@@ -26,7 +31,7 @@ void printLinkedList(struct linkedList list){
     }
 }
 
-
+//Inserts element in the beginning
 void insert(struct linkedList *list, int cardValue, enum suitType suit, bool visible){
     struct node *newNodeLink = (struct node*) malloc(sizeof (struct node));
     newNodeLink->cardValue = cardValue;
@@ -35,13 +40,14 @@ void insert(struct linkedList *list, int cardValue, enum suitType suit, bool vis
     newNodeLink->visible = visible;
     list->head = newNodeLink;
 }
-
+//Delete first (youngest) element
 struct node* deleteFirst(struct linkedList *list){
     struct node *deletedElement = list->head;
     list->head = list->head->next;
     return deletedElement;
 }
 
+//Returns pointer to node(card) that matches card value and suit
 struct node* findElement(struct linkedList list, int cardValue, enum suitType suit){
     struct node* el = list.head;
 
@@ -59,6 +65,7 @@ struct node* findElement(struct linkedList list, int cardValue, enum suitType su
     return el;
 }
 
+//Deletes element that matches card value and suit
 struct node* deleteElement(struct linkedList *list, int cardValue, enum suitType suit){
     struct node* el = list->head;
     struct node* prev = NULL;
@@ -84,8 +91,9 @@ struct node* deleteElement(struct linkedList *list, int cardValue, enum suitType
 
 
 }
-struct linkedList cardDeck;
-
+//7 linked lists one for each column.
+//Maybe it would be better to just have one??
+// But then it might make some operations a bit harder idk
 struct linkedList C1;
 struct linkedList C2;
 struct linkedList C3;
@@ -94,6 +102,14 @@ struct linkedList C5;
 struct linkedList C6;
 struct linkedList C7;
 
+//last command
+char lastCommand[20];
+
+//Status message changed by the handleInput
+char* status;
+
+// Takes a node struct (card) and changes a str to match the representation of the card.
+// str is the pointer to the string. E.g. 7 of spades becomes 7S.
 void cardToString(char *str,struct node *card){
     if(card == NULL){
         return;
@@ -133,6 +149,8 @@ void cardToString(char *str,struct node *card){
 
 }
 
+
+//Prints the game like in the project 2 pdf description.
 void printGameBoard(){
     printf("C1\tC2\tC3\tC4\tC5\tC6\tC7\n");
     bool hasMoreCards = true;
@@ -199,56 +217,165 @@ void printGameBoard(){
         index++;
     }
 
-    printf("\nLAST Command: %s\nMessage: %s", " ", " ");
+    printf("\nLAST Command: %s\nMessage: %s", lastCommand, status);
 
 
 }
 
-void loadCardDeck(char* name){
-    printf("LOAD");
-    FILE *filePointer = fopen("name", "r");
+//Reveals all cards in a column
+void showColumn(struct linkedList *col){
+    struct node *el = col->head;
+    while (el != NULL){
+        el->visible = true;
+        el = el->next;
+    }
+}
+
+//Reveals all cards
+char *showCards(){
+    showColumn(&C1);
+    showColumn(&C2);
+    showColumn(&C3);
+    showColumn(&C4);
+    showColumn(&C5);
+    showColumn(&C6);
+    showColumn(&C7);
+    return "OK";
+}
+
+
+// Loads a file and puts the cards in the columns. First horizontal from left to right and then vertical from top to bottom.
+// Right now they get inserted in reversed order (can be fixed by changing the insert() method)
+char* loadCardDeck(char* name){
+
+    if (access(name, F_OK) != 0) {
+        //Implement create new deck if file doesn't exists
+        return "Error: File doesn't exist";
+    }
+
+    FILE *filePointer = fopen(name, "r");
     char line[256];
+    int cardIndex = 1;
+
+    struct node *current1 = C1.head;
+    struct node *current2 = C2.head;
+    struct node *current3 = C3.head;
+    struct node *current4 = C4.head;
+    struct node *current5 = C5.head;
+    struct node *current6 = C6.head;
+    struct node *current7 = C7.head;
 
     while (fgets(line, sizeof (line), filePointer)){
-        printf("%s", line);
+       struct node *newCard = (struct node*) malloc(sizeof (struct node));
+        switch (line[0]) {
+            case 'A':
+                newCard->cardValue = 1;
+                break;
+            case 'T':
+                newCard->cardValue = 10;
+                break;
+            case 'J':
+                newCard->cardValue = 11;
+                break;
+            case 'Q':
+                newCard->cardValue=12;
+                break;
+            case 'K':
+                newCard->cardValue = 13;
+                break;
+            default:
+                newCard->cardValue = line[0]-'0';
+                break;
+        }
+
+
+        switch (line[1]) {
+            case 'H':
+                newCard->suit = hearts;
+                break;
+            case 'D':
+                newCard->suit = diamonds;
+                break;
+            case 'S':
+                newCard->suit = spades;
+                break;
+            case 'C':
+                newCard->suit = clubs;
+                break;
+        }
+
+
+        switch (cardIndex%7) {
+            case 1:
+                insert(&C1, newCard->cardValue, newCard->suit, false);
+                break;
+            case 2:
+                insert(&C2, newCard->cardValue, newCard->suit, false);
+                break;
+            case 3:
+                insert(&C3, newCard->cardValue, newCard->suit, false);
+                break;
+            case 4:
+                insert(&C4, newCard->cardValue, newCard->suit, false);
+                break;
+            case 5:
+                insert(&C5, newCard->cardValue, newCard->suit, false);
+                break;
+            case 6:
+                insert(&C6, newCard->cardValue, newCard->suit, false);
+                break;
+            default:
+                insert(&C7, newCard->cardValue, newCard->suit, false);
+                break;
+
+        }
+        cardIndex += 1;
+
     }
     fclose(filePointer);
-
-
+    return "OK";
 }
 
+// Should save the cards in the columns to a file
+char* saveCardDeck(char* filename){
+    return "NOT IMPLEMENTED";
+}
 
-
-void handleInput(){
+// Ask user for command and handles (some of it)
+int handleInput(){
     printf("\nINPUT > ");
-    char* in;
+    char in[20];
     gets(in);
+    strcpy(lastCommand, in);
+    char* comm = strtok(in, " ");
 
-    if(strcmp(in, "LD") == 0){
-        loadCardDeck("test");
+    if(strcmp(comm, "LD") == 0){
+        status = loadCardDeck(strtok(NULL, " "));
+    } else if(strcmp(comm, "SD") == 0){
+        status = saveCardDeck(strtok(NULL, " "));
+    } else if(strcmp(comm, "SW") == 0){
+        status = showCards();
+    }else if(strcmp(comm, "QQ") == 0){
+        return  1;
+    } else{
+        status = "Unknown command";
     }
+    return 0;
+}
+
+void commandLoop(){
+    bool quit = 0;
+    while (!quit){
+        printGameBoard();
+        quit = handleInput();
+    }
+
+
 }
 
 
 int main() {
-    insert(&C1, 1,spades, true);
-    insert(&C1, 2,spades, true);
-    insert(&C1, 5,hearts, true);
-    insert(&C1, 5,hearts, false);
-    insert(&C1, 5,hearts, false);
-
-    insert(&C2, 7, diamonds, true);
-    insert(&C2, 3, clubs, true);
-    insert(&C2, 1, spades, false);
-    insert(&C2, 1, spades, false);
-
-    insert(&C3, 10, diamonds, true);
-    insert(&C3, 11, clubs, true);
-    insert(&C3, 12, spades, false);
-    insert(&C3, 13, spades, false);
-    printGameBoard();
-
-    handleInput();
+    commandLoop();
 
     return 0;
 }
